fdc2fb9aabcf76af5222fda76dcc1a2f9c7527cd
Roberto Salgado <rsalgado03@gmail.com>
2015-04-06 03:04:38 +0000

    Add book creation functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,24 @@
 class BooksController < ApplicationController
   def index
     books = Book.all
-    
     if rating = params[:rating]
       books = books.where(rating: rating)
     end
     render json: books, status: 200
+  end
+  
+  def create
+    book = Book.new(book_params)
+    
+    if book.save
+      render json: book, status: 201, location: book
+    else
+      render json: book.errors, status: 422
+    end
+  end
+  
+  protected
+  def book_params
+    params.require(:book).permit(:title, :rating)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ActiveRecord::Base
+  scope :finished, ->{ where.not(finished_at: nil) }
+  
+  validates :title, presence: true
 end
