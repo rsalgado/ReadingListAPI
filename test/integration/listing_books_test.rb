24bd9f30ad30e8ccbaba89fa87129965b6f66fd8
Roberto Salgado <rsalgado03@gmail.com>
2015-04-06 02:12:20 +0000

    Refactor JSON parsing in tests

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -11,7 +11,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
-    assert_equal Book.count, JSON.parse(response.body).size
+    assert_equal Book.count, json(response.body).size
   end
   
   test "list top rated books" do
@@ -19,7 +19,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
-    assert_equal 1, JSON.parse(response.body).length
+    assert_equal 1, json(response.body).length
   end
   
 end
